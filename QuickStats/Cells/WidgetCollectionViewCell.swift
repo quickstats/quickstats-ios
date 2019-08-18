dafbe3c7381fc21044d6bc178ee21476474b869d
Paul Traylor <kungfudiscomonkey@gmail.com>
2019-08-18 11:33:27 +0900

    Countdown timer for widgets

--- QuickStats/Cells/WidgetCollectionViewCell.swift
+++ QuickStats/Cells/WidgetCollectionViewCell.swift
@@ -10,24 +10,30 @@ import UIKit
 import SDWebImage
 
 class WidgetCollectionViewCell: UICollectionViewCell {
+    var formatter: DateComponentsFormatter {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.day, .hour, .minute, .second]
+        formatter.unitsStyle = .positional
+        formatter.zeroFormattingBehavior = .dropLeading
+        return formatter
+    }
 
+    var timer: Timer?
     var widget: Widget? {
         didSet {
             titleLabel.text = widget?.title
 
             switch widget!.type {
             case .Countdown:
-                let formatter = DateComponentsFormatter()
-                formatter.allowedUnits = [.day, .hour, .minute, .second]
-                formatter.unitsStyle = .positional
-                formatter.zeroFormattingBehavior = .dropLeading
-                let duration = widget!.timestamp.timeIntervalSinceNow
-                valueLabel.text = formatter.string(from: duration)
+                timer = Timer.scheduledTimer(withTimeInterval: .init(1), repeats: true, block: updateCounter(timer:))
+                updateCounter(timer: timer!)
             default:
+                timer?.invalidate()
                 let formatter = NumberFormatter()
                 formatter.groupingSeparator = " "
                 formatter.numberStyle = .decimal
                 valueLabel.text = formatter.string(for: widget!.value)
+                valueLabel.textColor = UIColor.black
             }
         }
     }
@@ -37,12 +43,28 @@ class WidgetCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var iconView: UIImageView!
     @IBOutlet weak var valueLabel: UILabel!
 
+    func updateCounter(timer: Timer) {
+        let duration = widget!.timestamp.timeIntervalSinceNow
+        if duration > 0 {
+            valueLabel.text = formatter.string(from: duration)
+            valueLabel.textColor = UIColor.blue
+        } else {
+            valueLabel.text = formatter.string(from: duration * -1)
+            valueLabel.textColor = UIColor.red
+        }
+
+    }
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         layer.borderColor = UIColor.black.cgColor
         layer.borderWidth = 1
         layer.cornerRadius = 8
+    }
+
+    deinit {
+        timer?.invalidate()
     }
 
 }
