b6eb1c7198da2f2090f879e16688f99a94580883
Paul Traylor <kungfudiscomonkey@gmail.com>
2021-02-05 22:02:22 +0900

    Add color to widget border

--- QuickStats/Models/Widget.swift
+++ QuickStats/Models/Widget.swift
@@ -6,17 +6,41 @@
 //
 
 import Foundation
+import SwiftUI
+
+enum WidgetType: String, Codable {
+    case countdown = "Countdown"
+    case chart = "Chart"
+    case location = "Location"
+    case streak = "Streak"
+}
 
 struct Widget: Codable, Identifiable {
     var id: UUID
     var title: String
     var description: String
     var value: Double
+    var type: WidgetType
 
     struct List: Codable {
         var count: Int
         var next: URL?
         var prev: URL?
         var results: [Widget]
+    }
+}
+
+extension Widget {
+    var color: Color {
+        switch type {
+        case .chart:
+            return .green
+        case .countdown:
+            return .red
+        case .location:
+            return .blue
+        case .streak:
+            return .orange
+        }
     }
 }
--- QuickStats/Views/WidgetView.swift
+++ QuickStats/Views/WidgetView.swift
@@ -11,30 +11,26 @@ struct WidgetView: View {
     var widget: Widget
 
     var body: some View {
-        Group {
-            VStack(alignment: .center) {
-                Text(widget.title)
-                    .font(.title2)
-                Text(widget.description)
-                    .font(.caption)
-                Spacer()
-                Text("\(widget.value)")
-                    .font(.title)
-            }
-
-            //        .scaledToFit()
-            .minimumScaleFactor(0.5)
-            .lineLimit(1)
+        VStack(alignment: .center) {
+            Text(widget.title)
+                .font(.title2)
+            Text(widget.description)
+                .font(.caption)
+            Spacer()
+            Text(widget.value.description)
+                .font(.title)
         }
+        .minimumScaleFactor(0.5)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding()
-        .border(Color.blue, width: 5)
+        .border(widget.color, width: 5)
     }
 }
 
 struct WidgetView_Previews: PreviewProvider {
     static var previews: some View {
         WidgetView(
-            widget: .init(id: .init(), title: "test", description: "description", value: 100))
+            widget: .init(
+                id: .init(), title: "test", description: "description", value: 100, type: .streak))
     }
 }
