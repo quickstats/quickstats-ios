53e6db5527d66d4c8a190c3c4e5d40fe8b273888
Paul Traylor <kungfudiscomonkey@gmail.com>
2021-02-05 20:19:07 +0900

    Initial Commit

---
+++ QuickStats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuickStats
+//
+//  Created by Paul Traylor on 2021/02/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickStats/QuickStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickStatsApp.swift
+//  QuickStats
+//
+//  Created by Paul Traylor on 2021/02/05.
+//
+
+import SwiftUI
+
+@main
+struct QuickStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
